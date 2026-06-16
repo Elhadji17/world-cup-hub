2dbb91c11246513a603dd0e42a84cacc712b263f
Elhadji17 <haneelhadji04@gmail.com>
2026-06-16 15:52:55 +0200

    chore: force nouveau hash bundle

--- vite.config.js
+++ vite.config.js
@@ -7,29 +7,25 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-
     VitePWA({
       registerType: 'autoUpdate',
-
       manifest: {
         name: 'World Cup Hub',
         short_name: 'WCHub',
         description: 'Quiz et pronostics Coupe du Monde',
         theme_color: '#000000',
-
         icons: [
-          {
-            src: '/icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
+          { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/icon-512.png', sizes: '512x512', type: 'image/png' }
         ]
       }
     })
   ],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash]-v2.js`,
+      }
+    }
+  }
 })
