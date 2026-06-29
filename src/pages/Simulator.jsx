369191d26e1f690b49dbcb23741b65dbabe2c35d
Elhadji17 <haneelhadji04@gmail.com>
2026-06-29 06:41:13 +0200

    feat: Senegal-Belgique 1er juillet + timeline terrain tactique

--- src/pages/Simulator.jsx
+++ src/pages/Simulator.jsx
@@ -525,7 +525,6 @@ export default function Simulator() {
                 tacticId={tactic.id}
                 awayFlag={awayTeamFlag}
                 awayName={awayTeamName}
-                awayPlayers={awayPlayers}
                 timelineEvents={matchModule?.MATCH_TIMELINE_EVENTS ?? []} />
             </div>
             <div className="space-y-2">
